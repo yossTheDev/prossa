1e493b3ac0997b2c11383343eb2847e0682ad287
yoss <yossthedev@gmail.com>
2023-08-26 07:45:23 -0400

    Configure Android Project

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.yossthedev.rewolf;
+package com.example.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
 import { kApp, kPage, kProvider } from 'konsta/vue';
+import { useDark } from '@vueuse/core';
+import { onMounted } from 'vue';
+import { StatusBar, Style } from '@capacitor/status-bar';
+
+onMounted(() => {
+	useDark({
+		onChanged: (isDark) => {
+			if (isDark) {
+				StatusBar.setBackgroundColor({ color: '#1B1B1F' });
+				StatusBar.setStyle({ style: Style.Dark });
+			} else {
+				StatusBar.setBackgroundColor({ color: '#F1F3FC' });
+				StatusBar.setStyle({ style: Style.Light });
+			}
+		},
+	});
+});
 </script>
 
 <template>
