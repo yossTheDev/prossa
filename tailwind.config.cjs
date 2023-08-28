49a2091f47153242111bd9663978e3cb00be8bb0
yoss <yossthedev@gmail.com>
2023-08-28 14:05:23 -0400

    Update App Branding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ const konstaConfig = require('konsta/config');
 /** @type {import('tailwindcss').Config} */
 module.exports = konstaConfig({
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+
+	konsta: {
+		colors: {
+			primary: '#37CAD5',
+		},
+	},
+
 	theme: {
 		extend: {
 			colors: {
